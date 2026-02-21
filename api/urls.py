c796265e5b3d2c6f579f978ed4f29d192ac97de7
PavanNimkar <pavannnimkar@gmail.com>
2026-02-21 08:46:05 +0530

    GET and PUT using serializers

---
+++ api/urls.py
@@ -0,0 +1,4 @@
+from home.views import index, person
+from django.urls import path
+
+urlpatterns = [path("index", index), path("person", person)]
---
+++ home/views.py
@@ -0,0 +1,38 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import PersonSerializer
+from .models import Person
+
+
+# Create your views here.
+@api_view(["GET"])
+def index(request):
+    courses = {
+        "languages": ["python", "javascript"],
+        "duration": "3 months",
+        "price": 100,
+    }
+
+    return Response(courses)
+
+
+@api_view(["GET", "POST"])
+def person(request):
+    if request.method == "GET":
+        # query set data from database
+        objs = Person.objects.all()
+        # coverting this into JSON
+        serializer = PersonSerializer(objs, many=True)
+
+        return Response(serializer.data)
+
+    else:
+        # JSON data from frontend
+        data = request.data
+        # converting into queryset
+        serializer = PersonSerializer(data=data)
+        # validating data fields
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
