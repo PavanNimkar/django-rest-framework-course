19f5d1e943b6b9ec68c9ab812d56e4a70355fa9e
PavanNimkar <pavannnimkar@gmail.com>
2026-02-22 12:31:47 +0530

    DRF CRUD done

--- home/views.py
+++ home/views.py
@@ -16,7 +16,7 @@ def index(request):
     return Response(courses)
 
 
-@api_view(["GET", "POST"])
+@api_view(["GET", "POST", "PATCH", "PUT", "DELETE"])
 def person(request):
     if request.method == "GET":
         # query set data from database
@@ -26,7 +26,7 @@ def person(request):
 
         return Response(serializer.data)
 
-    else:
+    elif request.method == "POST":
         # JSON data from frontend
         data = request.data
         # converting into queryset
@@ -36,3 +36,26 @@ def person(request):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+
+    elif request.method == "PUT":
+        data = request.data
+        serializer = PersonSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    elif request.method == "PATCH":
+        data = request.data
+        obj = Person.objects.get(id=data["id"])
+        serializer = PersonSerializer(obj, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    else:
+        data = request.data
+        obj = Person.objects.get(id=data["id"])
+        obj.delete()
+        return Response({"message": "Object deleted"})
